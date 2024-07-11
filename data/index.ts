7d7a4b5e7def7a43025773729cc57e89bc9168c8
Nisar Saeed <dash75549@gmail.com>
2024-07-11 12:27:46 +0500

    file for encapsulating data from code created

---
+++ data/index.ts
@@ -0,0 +1,41 @@
+export const items = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication",
+    description: "",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tech enthusiast with a passion for development.",
+    description: "The Inside Scoop",
+    img: "/b5.svg",
+    spareImg: "",
+  },
+  {
+    id: 5,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    img: "",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    img: "",
+    spareImg: "",
+  },
+];
+
+
+export const leftLists = ["MERN", "TypeScript", "WordPress"];
+export const rightLists = ["MongoDB", "Next Js", "Postgres"];
