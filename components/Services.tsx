fff7d70c5a1cf37d28417b256bf5295fc8d27ab5
Nisar Saeed <dash75549@gmail.com>
2024-07-13 23:55:40 +0500

    update: service items position updated

--- components/Services.tsx
+++ components/Services.tsx
@@ -3,25 +3,27 @@ import { Highlight } from "./ui/Highlight-Text";
 import { serviceItems } from "@components/data";
 
 const hexStyle = {
-  clipPath:
-    " polygon(0% 25%,0% 75%,50% 100%,100% 75%,100% 25%,50% 0)",
+  clipPath: "polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%)"
 };
 
 const Services = () => {
   return (
-    <section id="services" className="h-screen w-full flex flex-col items-center">
-       <h1 className="md:text-5xl text-center w-full font-bold sm:text-4xl">
-          My<Highlight> services</Highlight>
-        </h1>
-      <div className="relative flex justify-center items-center h-[55%] w-1/3 mt-20">
+    <section
+      id="services"
+      className="h-fit w-screen flex flex-col  justify-center items-center relative py-20"
+    >
+      <h1 className="md:text-5xl text-center w-full font-bold sm:text-4xl">
+        My<Highlight> services</Highlight>
+      </h1>
+      <div className="relative flex justify-center items-center h-[450px] w-[1150px] mt-20">
         {serviceItems.map((item) => (
           <div
             key={item.id}
-            className={`absolute p-[2px] bg-purplePink inline-block ${item.position}  `}
+            className={`absolute p-[2px] bg-purplePink h-40 w-40 ${item.position}`}
             style={hexStyle}
           >
             <div
-              className="flex justify-center bg-lightLavender items-center h-40 w-44 flex-col text-black  p-2"
+              className="flex justify-center bg-lightLavender items-center h-full w-full flex-col text-black p-2"
               style={hexStyle}
             >
               <Image
@@ -29,15 +31,21 @@ const Services = () => {
                 width={100}
                 height={100}
                 alt="service section image"
-                className=" h-10 object-contain"
+                className="h-10 object-contain m-2"
               />
-              <div className="text-sm">{item.title}</div>
+              <div className="text-sm text-center">{item.title}</div>
             </div>
           </div>
         ))}
+        <div className="absolute p-[2px] h-40 w-40 transform -translate-x-1/2 -translate-y-1/2" style={{ top: "50%", left: "50%" }}>
+          <div className="flex justify-center bg-lightLavender items-center h-full w-full flex-col text-black p-2" style={hexStyle}>
+            <span className="text-3xl font-bold">NS</span>
+          </div>
+        </div>
       </div>
     </section>
   );
 };
 
 export default Services;
+
