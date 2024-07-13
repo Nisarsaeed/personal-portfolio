59cb9406e7ef2b87a3eba34244f068029d4e150f
Nisar Saeed <dash75549@gmail.com>
2024-07-13 20:36:22 +0500

    project, navbar and  servies data added

--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,4 @@
-export const items = [
+export const bentoGridItems = [
   {
     id: 1,
     title: "I prioritize client collaboration, fostering open communication",
@@ -36,6 +36,87 @@ export const items = [
   },
 ];
 
-
 export const leftLists = ["MERN", "TypeScript", "WordPress"];
 export const rightLists = ["MongoDB", "Next Js", "Postgres"];
+
+export const projectItems = [
+  {
+    title: "Real Time Chat Application",
+    description:
+      "Chat Application built using MERN Stack. Having all the functionalities",
+  },
+  {
+    title: "Dynamic To-do App",
+    description:
+      "Chat Application built using MERN Stack. Having all the functionalities",
+  },
+  {
+    title: "Dashboard ",
+    description:
+      "Chat Application built using MERN Stack. Having all the functionalities",
+  },
+  {
+    title: "Portfolio Project",
+    description:
+      "Chat Application built using MERN Stack. Having all the functionalities",
+  },
+];
+
+export const navItems = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Projects",
+    link:"#projects"
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+];
+
+export const serviceItems = [
+  {
+    id: 1,
+    img: "/s1.png",
+    title: "Frontend devlopment",
+    position: "top-0 left-1/3",
+  },
+  {
+    id: 2,
+    img: "/s2.png",
+    title: "Backend devlopment",
+    position: "top-1/4 left-0",
+  },
+  {
+    id: 3,
+    img: "/s3.png",
+    title: "Website devlopment",
+    position: "top-1/4 left-2/3",
+  },
+  {
+    id: 4,
+    img: "/s4.png",
+    title: "Website devlopment",
+    position: "top-3/4 left-0",
+  },
+  {
+    id: 5,
+    img: "/s5.png",
+    title: "Website devlopment",
+    position: "top-full left-1/3",
+  },
+  {
+    id: 6,
+    img: "/s6.png",
+    title: "Website devlopment",
+    position: "top-3/4 left-2/3",
+  },
+  // 'top-1/2 left-1/3',
+];
