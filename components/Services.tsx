ee8360ade190d50873de26c8f0f5357ac73877fe
Nisar Saeed <dash75549@gmail.com>
2024-07-14 13:16:39 +0500

    feat: responsiveness added to the service items

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Highlight } from "./ui/Highlight-Text";
-// import { serviceItems } from "@components/data";
 
 const hexStyle = {
   clipPath: "polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%)"
@@ -15,15 +14,15 @@ const Services = () => {
       <h1 className="md:text-5xl text-center w-full font-bold sm:text-4xl">
         My<Highlight> services</Highlight>
       </h1>
-      <div className="relative h-[450px] w-[1150px] mt-20">
+      <div className="relative h-[330px] w-[1030px] md:h-[450px] md:w-[1150px] lg:h-[900px] lg:w-[1500px] mt-20 ">
         {serviceItems.map((item) => (
           <div
             key={item.id}
-            className={`absolute p-[2px] bg-purplePink h-40 w-40 ${item.position}`}
+            className={`absolute p-[2px] bg-purplePink cursor-pointer h-28 w-28 md:h-40 md:w-40 lg:h-80 lg:w-80 ${item.position}`}
             style={hexStyle}
           >
             <div
-              className="flex justify-center bg-lightLavender items-center h-full w-full flex-col text-black p-2"
+              className="flex justify-center font-medium bg-lightLavender hover:bg-white hover:font-extrabold items-center h-full w-full flex-col text-black p-2"
               style={hexStyle}
             >
               <Image
@@ -31,13 +30,13 @@ const Services = () => {
                 width={100}
                 height={100}
                 alt="service section image"
-                className="h-10 object-contain m-2"
+                className="h-7 md:h-10 lg:h-20 object-contain m-2"
               />
-              <div className="text-sm text-center">{item.title}</div>
+              <div className="text-xs md:text-sm text-center lg:text-lg ">{item.title}</div>
             </div>
           </div>
         ))}
-        <div className="absolute p-[2px] h-40 w-40 transform -translate-x-1/2 -translate-y-1/2" style={{ top: "50%", left: "50%" }}>
+        <div className="absolute p-[2px] h-28 w-28 md:h-40 md:w-40 lg:h-80 lg:w-80 transform -translate-x-1/2 -translate-y-1/2" style={{ top: "50%", left: "50%" }}>
           <div className="flex justify-center bg-lightLavender items-center h-full w-full flex-col text-black p-2" style={hexStyle}>
             <span className="text-3xl font-bold">NS</span>
           </div>
@@ -66,13 +65,13 @@ const serviceItems = [
      id: 3,
      img: "/s3.png",
      title: "Responsive Design",
-     position: "top-[9em] left-[19em]",
+     position: " top-[7em] left-[21em] md:top-[9em] md:left-[19em] lg:top-[18em] lg:left-[14em]",
    },
    {
      id: 4,
      img: "/s4.png",
      title: "Database Management",
-     position: "top-[9em] right-[19em]",
+     position: "top-[7em] right-[21em] md:top-[9em] md:right-[19em] lg:top-[18em] lg:right-[14em]",
    },
    {
      id: 5,
@@ -85,6 +84,6 @@ const serviceItems = [
      img: "/s6.png",
      title: "Mobile App Development",
      position: "bottom-0 right-[25em]",
-   },
+   }
 ];
 
