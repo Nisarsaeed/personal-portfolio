230260865c1496b8352a8c5b02eca799c163d46a
Nisar Saeed <dash75549@gmail.com>
2024-07-14 18:53:46 +0500

    all components imported in the page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import Hero from "@components/components/Hero";
+import About from "@components/components/About";
+import Projects from "@components/components/Projects";
+import FloatingNav  from "@components/components/Floating-Navbar";
+import Footer from "@components/components/Footer";
+import Services from "@components/components/Services";
+
 export default function Home() {
   return (
-    <main className=" min-h-screen ">
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <FloatingNav/>
     <Hero/>
+    <About/>
+    <Projects />
+    <Services/>
+    <Footer />
     </main>
   );
 }
