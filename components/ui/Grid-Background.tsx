fac88ecbdb949218c1d75fe2050804644f05b701
Nisar Saeed <dash75549@gmail.com>
2024-07-15 15:19:55 +0500

    fix: pointer events enabled

--- components/ui/Grid-Background.tsx
+++ components/ui/Grid-Background.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function GridBackgroundDemo() {
   return (
     <div className="h-full w-full bg-grid-purplePink/[0.3] absolute [mask-image:radial-gradient(ellipse_at_center,transparent_30%,black)]">
-      <div className="pointer-events-none inset-0 flex items-center justify-center bg-darkBlue"></div>
+      <div className="inset-0 flex items-center justify-center bg-darkBlue"></div>
     </div>
   );
 }
